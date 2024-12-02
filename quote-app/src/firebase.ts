77467fea48ed232e25af618ff7a60428e1a6a033
Turdugul <okonbaevaturdugul@gmail.com>
2024-12-02 00:52:12 +0000

    env

--- quote-app/src/firebase.ts
+++ quote-app/src/firebase.ts
@@ -4,16 +4,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzRl1n37t51ElgkFIr9QQvY0Q6VNG1u04",
-  authDomain: "quotes-app-9b456.firebaseapp.com",
-  projectId: "quotes-app-9b456",
-  storageBucket: "quotes-app-9b456.firebasestorage.app",
-  messagingSenderId: "199115399958",
-  appId: "1:199115399958:web:7201dedb2ad6810027a637",
-  measurementId: "G-512SYBKFHM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
